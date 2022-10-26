5ea161ccd1794266d283f1b9388dc8792b5e2486
x1_medusa_1x <alin.iova@roweb.ro>
2022-10-26 17:19:20 +0300

    First Commit

---
+++ TD/src/cup/Driver.java
@@ -0,0 +1,12 @@
+package cup;
+
+import java_cup.runtime.*;
+
+class Driver {
+
+	public static void main(String[] args) throws Exception {
+		Parser parser = new Parser();
+		parser.parse();
+	}
+	
+}
